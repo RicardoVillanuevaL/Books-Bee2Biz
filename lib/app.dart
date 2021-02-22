9748a4399de1840807bfff0620388f23122f4092
Ricardo Villanueva <r.k.villanueva.laurente@gmail.com>
2021-02-22 02:36:02 -0500

    Create Splash Screen and add librarys-pubspec

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,9 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+
+//DESING
+import 'package:flutter_bees2biz/src/constants/colors.dart';
+import 'package:flutter_bees2biz/src/constants/images.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,14 +15,48 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Books Bee2Biz',
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-            child: Text('Bee2Biz'),
+      debugShowCheckedModeBanner: false,
+      home: ScreenSplash(),
+    );
+  }
+}
+
+class ScreenSplash extends StatefulWidget {
+  const ScreenSplash({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _ScreenSplashState createState() => _ScreenSplashState();
+}
+
+class _ScreenSplashState extends State<ScreenSplash> {
+  @override
+  void initState() {
+    loadNavigator();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: colorAzulPrimario,
+      body: Center(
+        child: ZoomIn(
+          child: Image.asset(
+            logoBee2Biz,
+            width: size.width / 1.5,
           ),
         ),
       ),
     );
+  }
+
+  void loadNavigator() {
+    Future.delayed(Duration(seconds: 2), () {
+      print('SE VA PARA EL CATALOGO DE LIBROS');
+      //Navigator.of(context).push(MaterialPageRoute(builder: (context)=> BooksPage();));
+    });
   }
 }
---
+++ lib/src/constants/images.dart
@@ -0,0 +1,3 @@
+final logoBee2Biz = 'assets/logo-bee2biz.png';
+final icon1 = 'assets/book.png';
+final icon2 = 'assets/books.png';
